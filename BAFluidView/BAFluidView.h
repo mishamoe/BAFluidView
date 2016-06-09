04411533731850ad650a5c177b13e1a73fcf5536
Mikhail Moyseyenko <mishamoee@gmail.com>
2016-06-09 16:00:27 +0300

    Moved animationDidStop

--- BAFluidView/BAFluidView.h
+++ BAFluidView/BAFluidView.h
@@ -16,3 +16,8 @@ FOUNDATION_EXPORT const unsigned char BAFluidViewVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <BAFluidView/PublicHeader.h>
 #import <BAFluidView/BAFluidView.h>
+
+/**
+ Handles animation completion
+ */
+@property (nonatomic, copy, nullable) void (^animationDidStop)();
